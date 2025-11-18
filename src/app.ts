145eef33a278da32ea2500a5fb4a6be23c5d0f4f
Clément <clement.dumont@saint-benigne.fr>
2025-11-18 10:31:11 +0100

    Refactor database migrations and schema for PostgreSQL compatibility
    - Removed old migration files for Users, Guilds, Challenges, Questions, Answer, and ResponseUser tables.
    - Added new migration file to create tables with updated schema definitions.
    - Altered Users table to allow nullable guildId and updated foreign key constraints.
    - Updated migration lock to switch from SQLite to PostgreSQL.
    - Modified Prisma schema to reflect changes in database provider and nullable fields.
    - Implemented user creation API with validation and error handling.
    - Created DTO schemas for user data validation and OpenAPI documentation.
    - Integrated login routes into the main application.
    - Adjusted middleware configuration for security and performance.

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import serverRoutes from '@/API/routes/server.routes';
 import challengeRoutes from '@/API/routes/challenges.routes';
 import questionRoutes from '@/API/routes/questions.route';
 import responseRoutes from '@/API/routes/responses.routes';
+import loginRoutes from '@/API/routes/login.routes'
 
 // API routes
 app.use('/api', serverRoutes);
@@ -14,5 +15,6 @@ app.use('/api', serverRoutes);
 app.use('/api/challenges', challengeRoutes);
 app.use('/api/questions', questionRoutes);
 app.use('/api/responses', responseRoutes);
+app.use('/api/login', loginRoutes);
 
 export default app;
